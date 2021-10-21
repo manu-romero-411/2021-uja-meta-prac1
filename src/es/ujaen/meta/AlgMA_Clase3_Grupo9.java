d322c83731b6ff567e7eb35ea8cd4dad41b6428c
amm00317 <amm00317@red.ujaen.es>
2021-10-21 17:03:18 +0200

    Mejorando codigo

--- src/es/ujaen/meta/AlgMA_Clase3_Grupo9.java
+++ src/es/ujaen/meta/AlgMA_Clase3_Grupo9.java
@@ -64,67 +64,8 @@ public class AlgMA_Clase3_Grupo9 {
         ArrayList<Integer> auxConjunto = conjunto;
         cambiaConjunto(par, auxConjunto);
 
-        boolean dlbCompleto = false;
-        int ultMov = 0;
-        int cam = 0;
-        int k = 0;
-        while (k < iteraciones && !dlbCompleto) {
-            int i = ultMov;
-            if (dlb.get(i % dlb.size()) == false) {
-                flagMejora = false;
-                int contJ = dlb.size() - 1;
-                for (int j = ((i + 1) % dlb.size()); contJ > 0 && flagMejora == false; j++) {
-                    if (i % dlb.size() != j) {
-                        if (checkMove(i % dlb.size(), j % dlb.size())) {
-                            applyMove(i % dlb.size(), j % dlb.size());
-                            dlb.set(i % dlb.size(), false);
-                            dlb.set(j % dlb.size(), false);
-                            flagMejora = true;
-                            ultMov = conjunto.get(j % dlb.size());
-                            cam++;
-                        }
-                    }
-                    contJ--;
-                }
-
-                if (flagMejora == false) {
-                    dlb.set(i % dlb.size(), true);
-                }
-            }
-            if (compruebaDLB()) {
-                dlbCompleto = true;
-            }
-            ultMov = (ultMov + 1) % conjunto.size();
-            k++;
-        }
-        System.out.println("ITERACIONES BUENAS: " + cam);
-
-    }
-    
-     private boolean checkMove(int r, int s) {
-        int matrizF[][] = archivo.getMatriz1();
-        int matrizD[][] = archivo.getMatriz2();
-        int sum = 0;
-
-        for (int k = 0; k < matrizF.length; k++) {
-            if (k != r && k != s) {
-                sum += ((matrizF[s][k] * (matrizD[conjunto.get(r)][conjunto.get(k)] - matrizD[conjunto.get(s)][conjunto.get(k)]))
-                        + (matrizF[r][k] * (matrizD[conjunto.get(s)][conjunto.get(k)] - matrizD[conjunto.get(r)][conjunto.get(k)]))
-                        + (matrizF[k][s] * (matrizD[conjunto.get(k)][conjunto.get(r)] - matrizD[conjunto.get(k)][conjunto.get(s)]))
-                        + (matrizF[k][r] * (matrizD[conjunto.get(k)][conjunto.get(s)] - matrizD[conjunto.get(k)][conjunto.get(r)])));
-            }
-        }
-        return (sum < 0);
     }
 
-    // Aplica el movimiento de mejora
-    private void applyMove(int r, int s) {
-        int valorR = conjunto.get(r);
-        conjunto.set(r, conjunto.get(s));
-        conjunto.set(s, valorR);
-    }
-    
-    
     private boolean compruebaDLB() {
         for (int i = 0; i < dlb.size(); i++) {
             if (!dlb.get(i)) {
